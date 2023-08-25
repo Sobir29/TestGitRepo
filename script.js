b8fffac102a0c3b9f5cfaba13892b422f4af2c50
Sobirjon <99137023+Sobir29@users.noreply.github.com>
2023-08-26 00:49:42 +0500

    upload files

---
+++ script.js
@@ -0,0 +1,21 @@
+// Function expression
+// Можно вызвпть до её создания
+function fn1(){
+    let a = 1;
+    console.log(a)
+}
+fn1()
+//Function declaration
+let fn2 = function(){
+    let a = 1;
+    console.log(a)
+}
+
+// Arrow function
+let fn3 = () => {
+    let a = 1;
+    console.log(a)
+}
+
+
+    
